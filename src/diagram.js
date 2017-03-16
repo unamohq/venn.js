46abb0dcac1415556008b6c5b9f0db907faf61cc
Kasia Gardocka <kasia.gardocka@positionly.com>
2017-03-16 12:05:30 +0100

    Give me more colors

--- src/diagram.js
+++ src/diagram.js
@@ -26,7 +26,7 @@ export function VennDiagram() {
         // so this is the same as d3.schemeCategory10, which is only defined in d3 4.0
         // since we can support older versions of d3 as long as we don't force this,
         // I'm hackily redefining below. TODO: remove this and change to d3.schemeCategory10
-        colourScheme = ["#1BB471", "#00BAAD", "#05BADF", "#1692EE", "#7F60CD"],
+        colourScheme = ["#1BB471", "#00BAAD", "#05BADF", "#1692EE", "#7F60CD", "#FA9D2B", "#FFC500", "#D4E04F", "#F75C91", "#F56565", "#E25151"],
         colourIndex = 0,
         colours = function(key) {
             if (key in colourMap) {
